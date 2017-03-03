e9c501b11e779ca5bd68d063a0e0abae48bac36b
osgee <ft.nudt@gmail.com>
2017-03-03 10:35:31 +0800

    fix bugs

--- opencv_installer.sh
+++ opencv_installer.sh
@@ -369,7 +369,7 @@ function install_opencv {
 	    -D INSTALL_PYTHON_EXAMPLES=ON \
 	    -D INSTALL_C_EXAMPLES=OFF \
 	    -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib-$OPENCV_VERSION/modules \
-	    -D PYTHON_EXECUTABLE=~/.virtualenvs/cv/bin/python \
+	    -D PYTHON_EXECUTABLE=~/.virtualenvs/$VE_DIR/bin/python \
 	    -D BUILD_EXAMPLES=ON ..
 
 	make -j4
